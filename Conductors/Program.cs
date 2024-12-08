0f4c9f63cd90a3d82a79327691646cfa75f0e89b
hp <akshayshende2700@gmail.com>
2024-12-08 17:58:33 +0530

    part1 -Added Projects

---
+++ Conductors/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Conductors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Data.mySql/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.mySql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
